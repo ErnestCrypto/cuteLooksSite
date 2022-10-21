cf111947dfb737f65417984948e0ae575dbe0248
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-10-21 18:24:32 +0000

    commit

--- CuteLooks/settings.py
+++ CuteLooks/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rxutpsv8k)*1bv^86l_f1fzbi9%)0m)izsa#a5_m3ip1%kdjm_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
