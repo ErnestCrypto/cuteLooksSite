3ce99174ab1115e760f21ad0d946cd288b73a54f
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-12-05 12:41:04 +0000

    commit

--- siteApp/views.py
+++ siteApp/views.py
@@ -156,7 +156,7 @@ def bookingPage(request):
         booking_specification = request.POST.get('booking_specification')
         subject = 'Client Bookings'
         cleint_subject = 'Bookings Confirmed'
-        message = f"Hello, please my name is {username} and I would like to book the {booking_type}, {booking_specification} makeup pakage. I can be contacted on {phone}"
+        message = f"Hello, please my name is {username} and I would like to book the {booking_type}, {booking_specification} makeup package. I can be contacted on {phone}"
         client_message = f"Hi,{username}.Your booking has been received successfully. You will be contacted on {phone} shortly. "
         email_from = settings.EMAIL_HOST_USER
         recipient_list = ['cutelooksgh@gmail.com']
