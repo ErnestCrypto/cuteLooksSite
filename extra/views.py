b2c01a545b0f5f7899b9f276db032d4ac354dc25
ErnestCrypto <akotobamfo.eab@gmail.com>
2023-01-10 15:42:45 +0000

    email sending done

--- extra/views.py
+++ extra/views.py
@@ -7,3 +7,26 @@
 # email_from = settings.EMAIL_HOST_USER
 # recipient_list = ['cutelooksgh@gmail.com']
 # client_list = [email]
+
+ # confirm_message = f"Dear, {firstName} {lastName}, your email has been received successfully. Kindly contact us on our whatsapp number 024 397 1445"
+# confrim_subject = "Email Recieved Successfully."
+# send_mail(subject, message_send, email, cutelooksWebsite)
+# send_mail(confrim_subject, confirm_message,
+#           cutelooksWebsitemail, email_from)
+
+
+# email_from = [email]
+# cutelooksWebsite = ["cutelooksgh@gmail.com"]
+# cutelooksWebsitemail = "cutelooksgh@gmail.com"
+
+
+# send_mail(subject, f_message, email, recipient_list)
+# send_mail(confirm_subject, confirm_message,
+# #           cutelooksWebsite, email_from)
+
+# f_message = f"{message}. My whatsapp number is {whatsapp}"
+# recipient_list = ['cutelooksgh@gmail.com']
+# email_from = [email]
+# cutelooksWebsite = "cutelooksgh@gmail.com"
+# confirm_subject = "Email Recieved Successfully."
+# confirm_message = f"Dear, {username}.Your email has been received successfully. Kindly get in touch with us through our whatsapp number 024 397 1445 for better assistance. Thank you"
--- siteApp/views.py
+++ siteApp/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-# from twilio.rest import Client
+
 
 def loginPage(request):
     context = {}
@@ -35,20 +35,54 @@ def indexPage(request):
     if request.method == 'POST':
         firstName = request.POST.get('firstName')
         lastName = request.POST.get('lastName')
+        username = firstName + ' ' + lastName
         whatsapp = request.POST.get('whatsapp')
         subject = request.POST.get('subject')
         message = request.POST.get('message')
-        message_send = f"{message}. My whatsapp number is {whatsapp}"
-        email = request.POST.get('email')
-        email_from = [email]
-        cutelooksWebsite = ["cutelooksgh@gmail.com"]
-        cutelooksWebsitemail = "cutelooksgh@gmail.com"
+        email_client = request.POST.get('email')
 
-        confirm_message = f"Dear, {firstName} {lastName}, your email has been received successfully. Kindly contact us on our whatsapp number 024 397 1445"
-        confrim_subject = "Email Recieved Successfully."
-        send_mail(subject, message_send, email, cutelooksWebsite)
-        send_mail(confrim_subject, confirm_message,
-                  cutelooksWebsitemail, email_from)
+        try:
+
+            # email to client
+            context = {
+                "username": username,
+                "email": email_client,
+                "phone": whatsapp,
+                "index": 'index',
+                "message": message
+            }
+            html_content = render_to_string("email.html", context)
+            text_content = strip_tags(html_content)
+            email = EmailMultiAlternatives(
+                subject,
+                text_content,
+                settings.EMAIL_HOST_USER,
+                [email_client]
+            )
+            email.attach_alternative(html_content, 'text/html')
+            email.send()
+            # email to cutelooks studios
+            content = {
+                "username": username,
+                "email": email_client,
+                "phone": whatsapp,
+                "index": 'index',
+                "message": message
+            }
+            html_content2 = render_to_string("receive.html", content)
+            text_content2 = strip_tags(html_content2)
+            email2 = EmailMultiAlternatives(
+                "Client Experience From CuteLooks Website",
+                text_content2,
+                settings.EMAIL_HOST_USER,
+                ["cutelooksgh@gmail.com"]
+            )
+            email2.attach_alternative(html_content2, 'text/html')
+            email2.send()
+
+        except:
+            return HttpResponse("Email not successful")
+
         return redirect('siteAppUrls:confirmPage')
 
     return render(request, 'index.html', context)
@@ -101,20 +135,54 @@ def contactPage(request):
     }
     if request.method == 'POST':
         username = request.POST.get('username')
-        email = request.POST.get('email')
+        email_client = request.POST.get('email')
         whatsapp = request.POST.get('whatsapp')
         subject = request.POST.get('subject')
         message = request.POST.get('message')
-        f_message = f"{message}. My whatsapp number is {whatsapp}"
-        recipient_list = ['cutelooksgh@gmail.com']
-        email_from = [email]
-        cutelooksWebsite = "cutelooksgh@gmail.com"
-        confirm_subject = "Email Recieved Successfully."
-        confirm_message = f"Dear, {username}.Your email has been received successfully. Kindly get in touch with us through our whatsapp number 024 397 1445 for better assistance. Thank you"
-        send_mail(subject, f_message, email, recipient_list)
-        send_mail(confirm_subject, confirm_message,
-                  cutelooksWebsite, email_from)
-        return redirect('siteAppUrls:confirmPage')
+        
+        try:
+            # email to client
+            context = {
+                "username": username,
+                "email":  email_client,
+                "phone": whatsapp,
+                "contact":"contact",
+                "message": message,
+
+            }
+            html_content = render_to_string("email.html", context)
+            text_content = strip_tags(html_content)
+            email = EmailMultiAlternatives(
+                subject,
+                text_content,
+                settings.EMAIL_HOST_USER,
+                [email_client]
+            )
+            email.attach_alternative(html_content, 'text/html')
+            email.send()
+            # email to cutelooks studios
+            content = {
+                "username": username,
+                "email":  email_client,
+                "phone": whatsapp,
+                "contact": "contact",
+                "message": message,
+            }
+            html_content2 = render_to_string("receive.html", content)
+            text_content2 = strip_tags(html_content2)
+            email2 = EmailMultiAlternatives(
+                "Client Enquiries From CuteLooks Website",
+                text_content2,
+                settings.EMAIL_HOST_USER,
+                ["cutelooksgh@gmail.com"]
+            )
+            email2.attach_alternative(html_content2, 'text/html')
+            email2.send()
+            
+        
+            return redirect('siteAppUrls:confirmPage')
+        except:
+            return HttpResponse("Email not successful")
     else:
         pass
 
@@ -161,11 +229,12 @@ def bookingPage(request):
         booking_type = request.POST.get('booking_type')
         booking_specification = request.POST.get('booking_specification')
         try:
+            # email to client
             context = {
-                "username":username,
-                "email":email,
-                "phone":phone,
-                "booking_type":booking_type,
+                "username": username,
+                "email": email,
+                "phone": phone,
+                "booking_type": booking_type,
                 "booking_specification": booking_specification,
 
             }
@@ -179,7 +248,7 @@ def bookingPage(request):
             )
             email.attach_alternative(html_content, 'text/html')
             email.send()
-            # kkkkkkkkkkkkk
+            # email to cutelooks studios
             content = {
                 "username": username,
                 "email": email,
@@ -198,13 +267,7 @@ def bookingPage(request):
             )
             email2.attach_alternative(html_content2, 'text/html')
             email2.send()
-            # sms 
-            # message_to_broadcast = ("Have you played the incredible TwilioQuest "
-            #                         "yet? Grab it here: https://www.twilio.com/quest")
-            # client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-            # client.messages.create(to=phone,
-            #                        from_=settings.TWILIO_NUMBER,
-            #                        body=message_to_broadcast)
+
             return redirect('siteAppUrls:confirmPage')
         except:
             return HttpResponse("Email not successful")
@@ -229,10 +292,10 @@ def confirmPage(request):
 def emailPage(request):
     context = {
         "username": 'Ernest',
-        "email":'akotobamfo.eab@gmail.com',
+        "email": 'akotobamfo.eab@gmail.com',
         "phone": '02134122312',
         "booking_type": 'photoshoot',
         "booking_specification": 'full gram',
 
     }
-    return render(request, 'email.html',context)
+    return render(request, 'email.html', context)
